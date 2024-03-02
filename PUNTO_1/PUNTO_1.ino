4dc946f3a9378fd024f70f693e8495016b6d92ff
JorgeEstrada1 <geometrykoko@gmail.com>
2024-03-02 11:44:55 -0400

    Punto2

--- PUNTO_1/PUNTO_1.ino
+++ PUNTO_1/PUNTO_1.ino
@@ -1,28 +1,72 @@
+#define LED 5
+#define LED2 6
+#define LED3 7
 #define LED 13
-#define PULSADOR 3
+#define LED2 12
+#define LED3 11
+#define PULSADOR 7
 #define ARDUINO 9600
 #define TIEMPO 200
-int contar =0;
+uint8_t val=0;
+uint8_t apagar=0;
+uint8_t prender=0;
 
 void setup() {
 pinMode(LED, OUTPUT);
+pinMode(LED2, OUTPUT);
+pinMode(LED3, OUTPUT);
 pinMode(PULSADOR, INPUT_PULLUP);
 Serial.begin(ARDUINO);
-attachInterrupt(digitalPinToInterrupt(PULSADOR), contador, RISING);
 }
 
 void loop() {
-digitalWrite(LED, HIGH);
-delay(TIEMPO);
-digitalWrite(LED, LOW);
-delay(TIEMPO);
-Serial.println(contar);
-delay(TIEMPO);
+  digitalWrite(LED, HIGH);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+  delay(TIEMPO);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED3,LOW);
+  delay(TIEMPO);
+  digitalWrite(LED, LOW);
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,HIGH);
+  delay(TIEMPO);
+  val=digitalRead(PULSADOR);
+  Serial.println(val);
+  if(val && apagar == 0)
+    {
+      prender = 1- prender;
+      delay (300);
+    }
+    apagar = val;
+  if (prender){
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+  }
+  else{
+    digitalWrite(LED, HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    delay(TIEMPO);
+    digitalWrite(LED, LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    delay(TIEMPO);
+  }
 }
-
-void contador(){
-  contar++;
-
-}
-
 
